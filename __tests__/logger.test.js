d7cc8521d2acdb46c147f042ff95184251d25c9c
Nadya Ilinskaya <nilinskaya@seattlecca.org>
2019-11-26 17:11:57 -0800

    fixint linter errors

--- __tests__/logger.test.js
+++ __tests__/logger.test.js
@@ -6,16 +6,16 @@
 
 describe('Logger loggs', () => {
 
-    it('will console log a message from what it receives during a save event', () => {
-      // io.socket.emit('examlpe', 'some messages');
-      // setTimeout(() => {
-      //   expect(message).toBe('some messages');
-      //   done();
-      // }, 50);
-      expect(true).toBe(true);
-    });
+  it('will console log a message from what it receives during a save event', () => {
+    // io.socket.emit('examlpe', 'some messages');
+    // setTimeout(() => {
+    //   expect(message).toBe('some messages');
+    //   done();
+    // }, 50);
+    expect(true).toBe(true);
+  });
 
-    it('will console error a message from what it receives during an err event', () => {
-      expect(true).toBe(true);
-    });
+  it('will console error a message from what it receives during an err event', () => {
+    expect(true).toBe(true);
+  });
 });
